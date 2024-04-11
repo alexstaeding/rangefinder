933773ec438ffbe6fcf4b6d2dc70334576fa856e
Alexander Städing <alexanderstaeding@gmail.com>
2024-04-11 22:28:01 +0200

    Kademlia buckets

--- lib/src/main/scala/io/github/alexstaeding/offlinesearch/network/KademliaRouting.scala
+++ lib/src/main/scala/io/github/alexstaeding/offlinesearch/network/KademliaRouting.scala
@@ -1,5 +1,7 @@
 package io.github.alexstaeding.offlinesearch.network
+
 import java.util
+import scala.annotation.tailrec
 import scala.collection.mutable
 import scala.concurrent.Future
 
@@ -14,7 +16,10 @@ class KademliaRouting[V](
     },
 ) extends Routing[V] {
 
-  private val buckets: mutable.Seq[KBucket] = new util.ArrayList[]()
+  private val buckets: mutable.Buffer[KBucket] = new mutable.ArrayDeque[KBucket]()
+
+  /** The bucket for the zero distance
+    */
   private val homeBucket: KBucket = new KBucket
 
   private def bucketIndex(id: NodeId): Int = {
@@ -25,12 +30,57 @@ class KademliaRouting[V](
     firstByte * 8 + bitPrefix
   }
 
-  class KBucket {
-    val nodes: mutable.Seq[NodeInfo] = new mutable.ArrayDeque[NodeInfo]
-    val values: mutable.Seq[V] = new mutable.ArrayDeque[V]
+  private def moveEntries[T](
+      partitionIndex: Int,
+      source: mutable.Map[NodeId, T],
+      destination: mutable.Map[NodeId, T],
+  ): Unit =
+    source
+      .collect { case (key, _) if bucketIndex(key) == partitionIndex => key }
+      .foreach { key => destination.put(key, source.remove(key).get) }
+
+  @tailrec
+  private def ensureBucketSpace(index: Int): Option[KBucket] = {
+    if (buckets.size + 1 > index) {
+      if (buckets.isEmpty) {
+        Option.when(homeBucket.hasSpace)(homeBucket)
+      } else {
+        Option.when(buckets(index).hasSpace)(buckets(index))
+      }
+    } else {
+      // index > buckets.size
+      if (homeBucket.hasSpace) {
+        Some(homeBucket)
+      } else {
+        val newTail = new KBucket
+        moveEntries(buckets.size, homeBucket.nodes, newTail.nodes)
+        moveEntries(buckets.size, homeBucket.values, newTail.values)
+        buckets.addOne(newTail)
+        ensureBucketSpace(index)
+      }
+    }
   }
 
-  override def ping(id: NodeId): Future[Unit] = {}
+  private def putInBucket(id: NodeId, value: NodeInfo): Boolean = {
+    val index = bucketIndex(id)
+    ensureBucketSpace(index) match {
+      case Some(bucket) => {
+        bucket.nodes.put(id, value)
+        true
+      }
+      case None => false
+    }
+  }
+
+  private class KBucket {
+    val nodes: mutable.Map[NodeId, NodeInfo] = new mutable.HashMap[NodeId, NodeInfo]()
+    val values: mutable.Map[NodeId, V] = new mutable.HashMap[NodeId, V]()
+
+    def isFull: Boolean = nodes.size + values.size >= kSize
+    def hasSpace: Boolean = !isFull
+  }
+
+  override def ping(id: NodeId): Future[Unit] = ???
 
   override def store(id: NodeId, value: V): Future[Boolean] = ???
 
