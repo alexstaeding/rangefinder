c409699162ebfdd2c2bac21622a637d8bae7c951
Alexander Städing <alexanderstaeding@gmail.com>
2024-06-16 14:49:41 +0200

    Rework force graph and add node ids

--- visualizer/src/app/graph/forcegraph.tsx
+++ visualizer/src/app/graph/forcegraph.tsx
@@ -1,15 +1,15 @@
-"use client";
+'use client'
 
 import React, {useEffect, useRef} from 'react';
 import * as d3 from 'd3';
-import {SimulationNodeDatum} from "d3-force";
 import {Data, Link, Node} from './data';
+import {SimulationNodeDatum} from "d3-force";
 
 interface ForceGraphProps extends Data {
   onNodeClick: (node: Node) => void;
 }
 
-const ForceGraph: React.FC<ForceGraphProps> = ({nodes, links, onNodeClick }) => {
+const ForceGraph: React.FC<ForceGraphProps> = ({nodes, links, onNodeClick}) => {
   const svgRef = useRef<SVGSVGElement>(null);
   const width = 1200;
   const height = 800;
@@ -24,7 +24,7 @@ const ForceGraph: React.FC<ForceGraphProps> = ({nodes, links, onNodeClick }) => 
     svg.selectAll('*').remove();
 
     const simulation = d3.forceSimulation<Node, Link>(nodes)
-      .force('link', d3.forceLink<Node, Link>(links).id(d => d.id).distance(200))
+      .force('link', d3.forceLink<Node, Link>(links).id(d => d.id).distance(300))
       .force('charge', d3.forceManyBody().strength(-800))
       .force('x', d3.forceX())
       .force('y', d3.forceY());
@@ -35,31 +35,42 @@ const ForceGraph: React.FC<ForceGraphProps> = ({nodes, links, onNodeClick }) => 
       .selectAll('line')
       .data(links)
       .join('line')
-      // .attr('stroke-width', d => Math.sqrt(d.value)); // TODO: d.kbucket?
+    // .attr('stroke-width', d => Math.sqrt(d.value)); // TODO: d.kbucket?
 
     const node = svg.append('g')
-      .selectAll('circle')
+      .attr('class', 'nodes')
+      .selectAll('g.node')
       .data(nodes)
-      .join('circle')
-      .attr('stroke', '#fff')
-      .attr('stroke-width', 1.5)
-      .attr('r', 25)
+      .join('g')
+      .attr('class', 'node')
       .on('click', (event, d) => {
         onNodeClick(d);
-        d3.select(event.target)
+        d3.select(event.currentTarget)
+          .select('circle')
           .attr('stroke-width', 6)
           .attr('stroke', '#aff');
-      })
+      });
+
+    node.append('circle')
+      .attr('stroke', '#fff')
+      .attr('stroke-width', 1.5)
+      .attr('r', 50)
       .attr('fill', d => color(d.nodeType));
 
     node.append('title')
+      .text(d => d.id);
+
+    node.append('text')
+      .attr('text-anchor', 'middle')
+      .attr('fill', 'white')
+      .attr('dy', '.35em')
       .text(d => d.id);
 
     // @ts-ignore
     node.call(d3.drag<Element, Node>()
       .on('start', dragstarted)
       .on('drag', dragged)
-      .on('end', dragended))
+      .on('end', dragended));
 
     simulation.on('tick', () => {
       link
@@ -69,8 +80,7 @@ const ForceGraph: React.FC<ForceGraphProps> = ({nodes, links, onNodeClick }) => 
         .attr('y2', d => (d.target as SimulationNodeDatum).y || 0);
 
       node
-        .attr('cx', d => d.x || 0)
-        .attr('cy', d => d.y || 0);
+        .attr('transform', d => `translate(${d.x}, ${d.y})`);
     });
 
     function dragstarted(event: any) {
