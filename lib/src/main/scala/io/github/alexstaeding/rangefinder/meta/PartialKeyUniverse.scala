bf713f3801f4a2f0753410c19aa923dcd47d5aa8
Alexander Städing <alexanderstaeding@gmail.com>
2024-09-04 09:52:20 +0200

    Start working on extending PartialKeyUniverse

--- lib/src/main/scala/io/github/alexstaeding/rangefinder/meta/PartialKeyUniverse.scala
+++ lib/src/main/scala/io/github/alexstaeding/rangefinder/meta/PartialKeyUniverse.scala
@@ -3,4 +3,6 @@ package io.github.alexstaeding.rangefinder.meta
 trait PartialKeyUniverse[V] {
   def getRootKey(value: V): PartialKey[V]
   def getOverlappingRootKeys(key: PartialKey[V]): Seq[PartialKey[V]]
+  def splitOnce(key: PartialKey[V]): Seq[PartialKey[V]]
+  def split(key: PartialKey[V]): Seq[PartialKey[V]]
 }
--- lib/src/main/scala/io/github/alexstaeding/rangefinder/meta/StringPrefixPartialKeyUniverse.scala
+++ lib/src/main/scala/io/github/alexstaeding/rangefinder/meta/StringPrefixPartialKeyUniverse.scala
@@ -18,4 +18,43 @@ object StringPrefixPartialKeyUniverse extends PartialKeyUniverse[String] {
       (start to end).map(_.toString).map(PartialKey.ofString)
     }
   }
+
+  override def splitOnce(key: PartialKey[String]): Seq[PartialKey[String]] = {
+
+    // exclusive key always ends with '\uFFFF'
+    val startLen = key.startInclusive.length
+    val endLen = key.endExclusive.length - 1
+    val delta = Math.abs(startLen - endLen)
+
+    val sharedPrefix = key.startInclusive.zip(key.endExclusive)
+      .takeWhile { (a, b) => a == b }
+      .map { (a, _) => a }
+      .mkString
+
+    if (delta == 0) {
+      // simple case
+      val bottomRow = if (sharedPrefix.length < startLen) {
+        // bottom row is at index after shared prefix
+        sharedPrefix.length
+      } else {
+        // strings are equal, last index is bottom row
+        startLen - 1
+      }
+
+      (key.startInclusive.charAt(bottomRow) to key.endExclusive.charAt(bottomRow))
+        .flatMap { bottom =>
+          ('a' to 'z')
+            .map { c =>
+              // TODO: What if sharedPrefix contains bottom
+              PartialKey.ofString(sharedPrefix + bottom + c)
+            }
+        }
+
+
+
+    } else {}
+    ???
+  }
+
+  override def split(key: PartialKey[String]): Seq[PartialKey[String]] = ???
 }
